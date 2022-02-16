00cf7a4f6ff5e092bde9f02afb9f33db055fcc67
hongmoSung <tesung30@naver.com>
2022-02-16 20:23:38 +0900

    first commit

---
+++ src/main/java/me/whiteship/App.java
@@ -0,0 +1,16 @@
+package me.whiteship;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ClassLoader classLoader = App.class.getClassLoader();
+        System.out.println(classLoader);
+        System.out.println(classLoader.getParent());
+        System.out.println(classLoader.getParent().getParent());
+    }
+}
